177e64502e6c3306e36447211bbf4c22419c926d
wenlongliu <liuwenlong@111.com>
2023-09-06 18:20:45 +0800

    feat：升级依赖版本，使用uni-ui组件库

--- src/pages/my/my.vue
+++ src/pages/my/my.vue
@@ -14,20 +14,29 @@ const getData = async () => {
 </script>
 
 <template>
-  <view class="my">
-    <view>会员信息：{{ memberStore.profile }}</view>
-    <button @tap="
+  <button
+    @tap="
       memberStore.setProfile({
         nickname: '黑马先锋',
       })
-      " size="mini" plain type="primary">
-      保存用户信息
-    </button>
+    "
+    size="mini"
+    plain
+    type="primary"
+  >
+    保存用户信息
+  </button>
+  <view class="my">
+    <view>会员信息：{{ memberStore.profile }}</view>
     <button @tap="memberStore.clearProfile()" size="mini" plain type="warn">清理用户信息</button>
     <button @tap="getData()" size="mini" plain type="warn">测试请求</button>
   </view>
+  <uni-card>
+    <text>这是一个基础卡片示例，内容较少，此示例展示了一个没有任何属性不带阴影的卡片。</text>
+  </uni-card>
 </template>
 
 <style lang="scss">
-//
+.my {
+}
 </style>
